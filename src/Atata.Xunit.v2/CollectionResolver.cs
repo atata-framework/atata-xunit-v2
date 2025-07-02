9ea2d753f80e7e3f19ab8696b65689e7773c4551
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-07-02 18:47:23 +0200

    Add initial sources

---
+++ src/Atata.Xunit.v2/CollectionResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace Atata.Xunit;
+
+internal static class CollectionResolver
+{
+    internal static bool TryResolveCollectionName(Type type, [NotNullWhen(true)] out string? collectionName)
+    {
+        var collectionAttribute = CustomAttributeData.GetCustomAttributes(type)
+            .FirstOrDefault(x => x.AttributeType == typeof(CollectionAttribute));
+
+        if (collectionAttribute is not null)
+        {
+            collectionName = collectionAttribute.ConstructorArguments[0].Value as string;
+
+            return collectionName is not null;
+        }
+        else
+        {
+            collectionName = null;
+            return false;
+        }
+    }
+}
---
+++ test/Atata.Xunit.v2.IntegrationTests/SomeCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace Atata.Xunit.IntegrationTests;
+
+[CollectionDefinition(Name)]
+public sealed class SomeCollection : ICollectionFixture<SomeCollectionFixture>
+{
+    public const string Name = "Some collection";
+}
