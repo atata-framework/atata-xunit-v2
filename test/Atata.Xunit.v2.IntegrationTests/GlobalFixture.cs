ad5f3d7955d4abfb922c5db04cfbf7dc150ea6a9
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-08-31 21:03:33 +0200

    Add `AtataContext.GlobalProperties.RootNamespace` setting into `AtataGlobalFixture.InitializeAsync` method

--- test/Atata.Xunit.v2.IntegrationTests/GlobalFixture.cs
+++ test/Atata.Xunit.v2.IntegrationTests/GlobalFixture.cs
@@ -4,9 +4,6 @@ namespace Atata.Xunit.IntegrationTests;
 
 public sealed class GlobalFixture : AtataGlobalFixture
 {
-    protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
-        globalProperties.UseRootNamespaceOf<GlobalFixture>();
-
     protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder) =>
         builder.LogConsumers.AddNLogFile();
 }
